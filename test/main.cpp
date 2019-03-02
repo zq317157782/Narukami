6cec7dc8ae963d4642270cffdbf56934019d9e26
zhuqian <zq317157782@163.com>
2019-03-02 19:40:05 +0800

    update rcp test

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,9 @@
 using namespace narukami;
 TEST(Math,rcp){
     EXPECT_FLOAT_EQ(rcp(2.0f),0.5f);
+    EXPECT_FLOAT_EQ(rcp(3.0f),1.0f/3.0f);
+    EXPECT_FLOAT_EQ(rcp(5.0f),1.0f/5.0f);
+    EXPECT_FLOAT_EQ(rcp(7.0f),1.0f/7.0f);
 }
 
 int main(int argc, char* argv[]) {
