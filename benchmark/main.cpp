6b78e24cad1ba4d49e0699a2bfedeb349d131b00
zhuqian <zq317157782@163.com>
2019-03-02 19:28:50 +0800

    use rcp instead fast_rcp

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -1,5 +1,6 @@
 #include "benchmark.h"
 #include <xmmintrin.h>
+#include "math.h"
 
 static void BM_common_rcp(benchmark::State& state){
   float ret=0;
@@ -11,17 +12,17 @@ static void BM_common_rcp(benchmark::State& state){
 BENCHMARK(BM_common_rcp)->Range(8,8<<10);
 
 
-float fast_rcp(float x){
-    const __m128 a = _mm_set_ss(x);
-    const __m128 r = _mm_rcp_ss(a);
-    return _mm_cvtss_f32(_mm_mul_ss(r,_mm_sub_ss(_mm_set_ss(2.0f), _mm_mul_ss(r, a))));
-}
+// float fast_rcp(float x){
+//     const __m128 a = _mm_set_ss(x);
+//     const __m128 r = _mm_rcp_ss(a);
+//     return _mm_cvtss_f32(_mm_mul_ss(r,_mm_sub_ss(_mm_set_ss(2.0f), _mm_mul_ss(r, a))));
+// }
 
 static void BM_fast_rcp(benchmark::State& state){
     float ret=0;
     for (auto _ : state){
         float v=state.range(0);
-         benchmark::DoNotOptimize(ret+=fast_rcp(v));
+         benchmark::DoNotOptimize(ret+=rcp(v));
     }
 }
 
