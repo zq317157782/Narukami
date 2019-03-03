d903a227811269fc9d4d07a49377c96eef6666b6
zhuqian <zq317157782@163.com>
2019-03-03 14:13:48 +0800

    add MAYBE_UNUSED marco

--- src/core/platform.h
+++ src/core/platform.h
@@ -5,12 +5,14 @@
 #define NOINLINE               __attribute__((noinline))
 #define EXPECT_TAKEN(a)        __builtin_expect(!!(a), true)
 #define EXPECT_NOT_TAKEN(a)    __builtin_expect(!!(a), false)
+#define MAYBE_UNUSED           __attribute__((unused))
 #elif defined(__MSVC__)
 #define FINLINE                __forceinline
 #define NOINLINE               __declspec(noinline)
 #define MM_ALIGN16             __declspec(align(16))
 #define EXPECT_TAKEN(a)        (a)
 #define EXPECT_NOT_TAKEN(a)    (a)
+#define MAYBE_UNUSED           
 #else
 #error Unsupported compiler!
 #endif
