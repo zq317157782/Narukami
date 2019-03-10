c38b91be46c5f0897db542e290f22aace01ef1f4
zhuqian <zq317157782@163.com>
2019-03-10 21:19:16 +0800

    add vector2.h

---
+++ src/core/vector2.h
@@ -0,0 +1,243 @@
+/*
+MIT License
+
+Copyright (c) 2019 ZhuQian
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+#include "narukami.h"
+#include "core/math.h"
+
+NARUKAMI_BEGIN
+template <typename T>
+class Vector2
+{
+  public:
+    T x, y;
+
+    typedef T Scalar;
+    enum
+    {
+        N = 2
+    };
+
+  public:
+    FINLINE Vector2() : x(0), y(0)
+    {
+    }
+
+    FINLINE explicit Vector2(const float a) : x(a), y(a)
+    {
+        assert(!isnan(a));
+    }
+
+    FINLINE Vector2(const T &a, const T &b) : x(a), y(b)
+    {
+        assert(!isnan(a));
+        assert(!isnan(b));
+    }
+    //just for checking assert for debug
+#ifdef NARUKAMI_DEBUG
+    FINLINE Vector2(const Vector2 &v1)
+    {
+        assert(!isnan(v1.x));
+        assert(!isnan(v1.y));
+        x = v1.x;
+        y = v1.y;
+    }
+
+    FINLINE Vector2 &operator=(const Vector2 &v1)
+    {
+        assert(!isnan(v1.x));
+        assert(!isnan(v1.y));
+        x = v1.x;
+        y = v1.y;
+        return (*this);
+    }
+#endif
+    FINLINE Vector2 &operator+=(const Vector2 &v1)
+    {
+        x += v1.x;
+        y += v1.y;
+        return (*this);
+    }
+
+    FINLINE Vector2 &operator-=(const Vector2 &v1)
+    {
+        x -= v1.x;
+        y -= v1.y;
+        return (*this);
+    }
+
+    FINLINE Vector2 &operator*=(const Vector2 &v1)
+    {
+        x *= v1.x;
+        y *= v1.y;
+        return (*this);
+    }
+
+    FINLINE Vector2 &operator*=(const T &f)
+    {
+        x *= f;
+        y *= f;
+        return (*this);
+    }
+
+    FINLINE Vector2 &operator/=(const T &f)
+    {
+        assert(f != 0);
+        x /= f;
+        y /= f;
+        return (*this);
+    }
+
+    FINLINE T operator[](const int idx) const
+    {
+        assert(idx >= 0 && idx < 2);
+        return (&x)[idx];
+    }
+
+    FINLINE T &operator[](const int idx)
+    {
+        assert(idx >= 0 && idx < 2);
+        return (&x)[idx];
+    }
+
+    FINLINE friend std::ostream &operator<<(std::ostream &out, const Vector2 &v)
+    {
+        out << '(' << v.x << ',' << v.y  << ')';
+        return out;
+    }
+};
+
+typedef Vector2<float> Vector2f;
+typedef Vector2<int> Vector2i;
+
+template <typename T>
+FINLINE Vector2<T> operator+(const Vector2<T> &v1, const Vector2<T> &v2)
+{
+    Vector2<T> v;
+    v.x = v1.x + v2.x;
+    v.y = v1.y + v2.y;
+    return v;
+}
+
+template <typename T>
+FINLINE Vector2<T> operator-(const Vector2<T> &v1, const Vector2<T> &v2)
+{
+    Vector2<T> v;
+    v.x = v1.x - v2.x;
+    v.y = v1.y - v2.y;
+    return v;
+}
+
+//compenont wise
+template <typename T>
+FINLINE Vector2<T> operator*(const Vector2<T> &v1, const Vector2<T> &v2)
+{
+    Vector2<T> v;
+    v.x = v1.x * v2.x;
+    v.y = v1.y * v2.y;
+    return v;
+}
+
+template <typename T>
+FINLINE Vector2<T> operator*(const Vector2<T> &v1, const T &f)
+{
+    Vector2<T> v;
+    v.x = v1.x * f;
+    v.y = v1.y * f;
+    return v;
+}
+template <typename T>
+FINLINE Vector2<T> operator/(const Vector2<T> &v1, const T &f)
+{
+    assert(f != 0);
+    Vector2<T> v;
+    v.x = v1.x / f;
+    v.y = v1.y / f;
+    return v;
+}
+
+template <typename T>
+FINLINE bool operator==(const Vector2<T> &v1, const Vector2<T> &v2)
+{
+    if ((v1.x == v2.x) && (v1.y == v2.y))
+    {
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+FINLINE bool operator!=(const Vector2<T> &v1, const Vector2<T> &v2)
+{
+    if ((v1.x != v2.x) || (v1.y != v2.y))
+    {
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+FINLINE T dot(const Vector2<T> &v1, const Vector2<T> &v2)
+{
+    return v1.x * v2.x + v1.y * v2.y;
+}
+
+template <typename T>
+FINLINE float length(const Vector2<T> &v)
+{
+    return sqrt(float(dot(v, v)));
+}
+
+template <typename T>
+FINLINE float lensqr(const Vector2<T> &v)
+{
+    return float(dot(v, v));
+}
+
+template <typename T>
+FINLINE T cross(const Vector2<T> &v1, const Vector2<T> &v2)
+{
+    return v1.x * v2.y - v1.y * v2.x;
+}
+
+template <typename T>
+FINLINE Vector2f normalize(const Vector2<T> &v1)
+{
+    float inv_l = rsqrt(dot(v1, v1));
+    return v1 * inv_l;
+}
+
+template <typename T>
+FINLINE Vector2f rcp(const Vector2<T> &v1)
+{
+    return Vector2f(rcp(v1.x), rcp(v1.y));
+}
+
+template <typename T>
+FINLINE Vector2f sqrt(const Vector2<T> &v1)
+{
+    return Vector2f(sqrt(v1.x), sqrt(v1.y));
+}
+
+NARUKAMI_END
--- test/main.cpp
+++ test/main.cpp
@@ -69,7 +69,8 @@ TEST(math,lerp){
     EXPECT_FLOAT_EQ(a,0.5f);
 }
 
-#include "core/vector.h"
+#include "core/vector2.h"
+#include "core/vector3.h"
 // TEST(type,int_div_zero){
 //     int a= 1/0;
 //     EXPECT_EQ(isnan(a),false);
@@ -82,11 +83,25 @@ TEST(vector3f,add){
     EXPECT_EQ(v3,Vector3f(5,7,9));
 }
 
+TEST(vector2f,add){
+    Vector2f v1(1,2);
+    Vector2f v2(4,5);
+    auto v3=v1+v2;
+    EXPECT_EQ(v3,Vector2f(5,7));
+}
+
 TEST(vector3f,sub){
     Vector3f v1(1,2,3);
     Vector3f v2(4,5,6);
     auto v3=v1-v2;
     EXPECT_EQ(v3,Vector3f(-3,-3,-3));
+}
+
+TEST(vector2f,sub){
+    Vector2f v1(1,2);
+    Vector2f v2(4,5);
+    auto v3=v1-v2;
+    EXPECT_EQ(v3,Vector2f(-3,-3));
 }
 
 
@@ -97,12 +112,26 @@ TEST(vector3f,mul){
     EXPECT_EQ(v3,Vector3f(4,10,18));
 }
 
+TEST(vector2f,mul){
+    Vector2f v1(1,2);
+    Vector2f v2(4,5);
+    auto v3=v1*v2;
+    EXPECT_EQ(v3,Vector2f(4,10));
+}
+
 TEST(vector3f,mul2){
     Vector3f v1(1,2,3);
     float f=2; 
     auto v2=v1*f;
     EXPECT_EQ(v2,Vector3f(2,4,6));
 }
+TEST(vector2f,mul2){
+    Vector2f v1(1,2);
+    float f=2; 
+    auto v2=v1*f;
+    EXPECT_EQ(v2,Vector2f(2,4));
+}
+
 
 TEST(vector3f,div){
     Vector3f v1(1,2,3);
@@ -111,10 +140,25 @@ TEST(vector3f,div){
     EXPECT_EQ(v2,Vector3f(0.5f,1.0f,1.5f));
 }
 
+TEST(vector2f,div){
+    Vector2f v1(1,2);
+    float f=2; 
+    auto v2=v1/f;
+    EXPECT_EQ(v2,Vector2f(0.5f,1.0f));
+}
+
 TEST(vector3f,equal){
     Vector3f v1(1);
     Vector3f v2(2);
     Vector3f v3(2);
+    EXPECT_TRUE(v1!=v3);
+    EXPECT_TRUE(v2==v3);
+}
+
+TEST(vector2f,equal){
+    Vector2f v1(1);
+    Vector2f v2(2);
+    Vector2f v3(2);
     EXPECT_TRUE(v1!=v3);
     EXPECT_TRUE(v2==v3);
 }
@@ -127,10 +171,17 @@ TEST(vector3f,cross){
     EXPECT_EQ(v3,v);
 }
 
+
 TEST(vector3f,normalize){
     Vector3f v1(5,0,0);
     auto v2=normalize(v1);
     EXPECT_EQ(v2,Vector3f(1,0,0));
+}
+
+TEST(vector2f,normalize){
+    Vector2f v1(5,0);
+    auto v2=normalize(v1);
+    EXPECT_EQ(v2,Vector2f(1,0));
 }
 
 
@@ -141,10 +192,29 @@ TEST(vector3f,rcp){
     EXPECT_GE(v2.x,0.49999);
 }
 
+TEST(vector2f,rcp){
+    Vector2f v1(2,2);
+    auto v2=rcp(v1);
+    EXPECT_LE(v2.x,0.50001);
+    EXPECT_GE(v2.x,0.49999);
+}
+
 TEST(vector3f,subscript){
     Vector3f v1(0);
     v1[0]=1;
     EXPECT_FLOAT_EQ(v1[0],1);
+}
+
+TEST(vector2f,subscript){
+    Vector2f v1(0);
+    v1[0]=1;
+    EXPECT_FLOAT_EQ(v1[0],1);
+}
+
+TEST(vector2f,sqrt){
+    Vector2f v1(9);
+    auto v2=sqrt(v1);
+    EXPECT_EQ(v2,Vector2f(3,3));
 }
 
 TEST(vector3i,dot){
