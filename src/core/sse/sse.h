36ba1bbbc814ad9a30dff426173555ea43048192
zhuqian <zhuqian@fantablade.com>
2019-03-13 14:35:50 +0800

    add _sse_blendv_ps

--- src/core/sse/sse.h
+++ src/core/sse/sse.h
@@ -24,3 +24,11 @@ SOFTWARE.
 #pragma once
 #include <xmmintrin.h>//sse
 #include <emmintrin.h>//sse2
+
+
+#define SSE_ALIGNAS alignas(16)
+
+//SSE4.1's _mm_blendv_ps 
+__m128 _sse_blendv_ps(__m128 a, __m128 b, __m128 mask){
+    return _mm_or_ps(_mm_and_ps(mask,b),_mm_andnot_ps(mask,a));
+}
