fc29ab8c0fd80fcd12a70f9ab6df0a0dbe52c2b6
zhuqian <zhuqian@fantablade.com>
2019-03-15 10:12:00 +0800

    refactor Matrix4x4

--- src/core/math/matrix4x4.h
+++ src/core/math/matrix4x4.h
@@ -31,19 +31,94 @@ NARUKAMI_BEGIN
 //column  major
 struct Matrix4x4{
 public:
-    float4 c0,c1,c2,c3;
+    union{
+        float m[16];
+        __m128 mVec[4];
+    };
     typedef float Scalar;
     enum
     {
         N = 16
     };
 public:
-    Matrix4x4():c0(float4(1,0,0,0)),c1(float4(0,1,0,0)),c2(float4(0,0,1,0)),c3(float4(0,0,0,1)){}
-    Matrix4x4(const float4& c0,const float4&c1,const float4& c2,const float4&c3):c0(c0),c1(c1),c2(c2),c3(c3){}
-    FINLINE const float& operator[](const int idx) const { assert(idx >= 0 && idx < N); return (&c0.x)[idx];}
-    FINLINE float& operator[](const int idx){ assert(idx >= 0 && idx < N); return (&c0.x)[idx];}
+    Matrix4x4(){
+         //row 0
+        m[0]=1.0f;
+        m[1]=0.0f;
+        m[2]=0.0f;
+        m[3]=0.0f;
+        //row 1
+        m[4]=0.0f;
+        m[5]=1.0f;
+        m[6]=0.0f;
+        m[7]=0.0f;
+        //row 2
+        m[8]=0.0f;
+        m[9]=0.0f;
+        m[10]=1.0f;
+        m[11]=0.0f;
+        //row 3
+        m[12]=0.0f;
+        m[13]=0.0f;
+        m[14]=0.0f;
+        m[15]=1.0f;
+    }
+    Matrix4x4(const float m0,const float m1,const float m2,const float m3,const float m4,const float m5,const float m6,const float m7,const float m8,const float m9,const float m10,const float m11,const float m12,const float m13,const float m14,const float m15)
+    {
+        //row 0
+        m[0]=m0;
+        m[1]=m1;
+        m[2]=m2;
+        m[3]=m3;
+        //row 1
+        m[4]=m4;
+        m[5]=m5;
+        m[6]=m6;
+        m[7]=m7;
+        //row 2
+        m[8]=m8;
+        m[9]=m9;
+        m[10]=m10;
+        m[11]=m11;
+        //row 3
+        m[12]=m12;
+        m[13]=m13;
+        m[14]=m14;
+        m[15]=m15;
+    }
+
+    Matrix4x4(const float* v){
+        //row 0
+        m[0]=v[0];
+        m[1]=v[1];
+        m[2]=v[2];
+        m[3]=v[3];
+        //row 1
+        m[4]=v[4];
+        m[5]=v[5];
+        m[6]=v[6];
+        m[7]=v[7];
+        //row 2
+        m[8]=v[8];
+        m[9]=v[9];
+        m[10]=v[10];
+        m[11]=v[11];
+        //row 3
+        m[12]=v[12];
+        m[13]=v[13];
+        m[14]=v[14];
+        m[15]=v[15];
+    }
+    FINLINE const float& operator[](const int idx) const { assert(idx >= 0 && idx < N); return m[idx];}
+    FINLINE float& operator[](const int idx){ assert(idx >= 0 && idx < N); return m[idx];}
 };
 
-FINLINE std::ostream &operator<<(std::ostream &out, const Matrix4x4 &v){out << '(' << v.c0 << ',' << v.c1 << ',' << v.c2 << ',' << v.c3 << ')';return out;}
+FINLINE std::ostream &operator<<(std::ostream &out, const Matrix4x4 &v){
+    for(int i=0;i<Matrix4x4::N;++i){
+        out<<v.m[i];
+        out<<((i+1)==Matrix4x4::N)?')':',';
+    }
+    return out;
+}
 
 NARUKAMI_END
