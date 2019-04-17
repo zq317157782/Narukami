a310ed6023afa493fe4a9a802f4658ea7a6113c3
zhuqian <zhuqian@fantablade.com>
2019-04-17 15:39:13 +0800

    refactor benchmark

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -485,9 +485,10 @@ BENCHMARK(BM_matrix4x4_mul_point3_sse);
 
 
 static void BM_intersect_triangle(benchmark::State &state)
-{
+{   narukami::HitInfo hit;
      narukami::Ray r(narukami::Point3f(rand(),rand(),rand()),narukami::Vector3f(rand(),rand(),rand()));
     auto triangles = new narukami::Triangle[state.range(0)];
+    
     for(size_t i = 0; i < state.range(0); i++)
     {
        triangles[i].v0=narukami::Point3f(rand(),rand(),rand());
@@ -499,7 +500,7 @@ static void BM_intersect_triangle(benchmark::State &state)
 
         for(size_t i = 0; i < state.range(0); i++)
         {
-            benchmark::DoNotOptimize(intersect(r,triangles[i]));
+            benchmark::DoNotOptimize(intersect(r,triangles[i],&hit));
         }
         
     }
@@ -509,6 +510,7 @@ BENCHMARK(BM_intersect_triangle)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
 
 static void BM_intersect_soatriangle(benchmark::State &state)
 {
+    narukami::HitInfo hit;
     narukami::SoARay r(narukami::Point3f(rand(),rand(),rand()),narukami::Vector3f(rand(),rand(),rand()));
     auto triangles = new narukami::SoATriangle[state.range(0)]();
     for(size_t i = 0; i < state.range(0); i++)
@@ -522,7 +524,7 @@ static void BM_intersect_soatriangle(benchmark::State &state)
     {
         for(size_t i = 0; i < state.range(0); i++)
         {
-             benchmark::DoNotOptimize(intersect(r,triangles[i]));
+             benchmark::DoNotOptimize(intersect(r,triangles[i],&hit));
         }
     }
 
