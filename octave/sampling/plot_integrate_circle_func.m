5d0ef6db8efb31b9be5a98b187cf0fdfc7547c93
zhuqian <zhuqian@fantablade.com>
2019-05-23 17:04:22 +0800

    fix some bug for plot_integrate_circle_func

--- octave/sampling/plot_integrate_circle_func.m
+++ octave/sampling/plot_integrate_circle_func.m
@@ -1,19 +1,21 @@
 function plot_integrate_circle_func()
+    
     true_value = pi*0.25;
-    a_size = 50;
-    x = zeros(a_size);
-    halton_y = zeros(a_size);
-    r2_y = zeros(a_size);
-    stratified_y = zeros(a_size);
-    hammersley_y = zeros(a_size);
-    hypercube_y = zeros(a_size);
-    random_y = zeros(a_size);
+    a_size = 500;
+    x = 1:1:a_size;
+    halton_y = zeros(a_size,1);
+    r2_y = zeros(a_size,1);
+    stratified_y = zeros(a_size,1);
+    hammersley_y = zeros(a_size,1);
+    hypercube_y = zeros(a_size,1);
+    random_y = zeros(a_size,1);
     #设置样本数
-    x(1)=2;
-    for idx=2:a_size
-        x(idx)=idx*idx;
-    end
+    % for idx=1:a_size
+    %     x(idx)=idx;
+    % end
 
+
+   
 
     for idx=1:a_size
         N=x(idx);
@@ -26,6 +28,7 @@ function plot_integrate_circle_func()
         halton_y(idx)=abs(estimate_value-true_value);
     end
 
+
     for idx=1:a_size
         N=x(idx);
         sample_sum=0;
@@ -37,16 +40,16 @@ function plot_integrate_circle_func()
         r2_y(idx)=abs(estimate_value-true_value);
     end
 
-    for idx=1:a_size
-        N=x(idx);
-        sample_sum=0;
-        for u=1:N
-            [ux,uy]=stratified(u,sqrt(N),sqrt(N));
-            sample_sum = sample_sum+circle_func(ux,uy);
-        end
-        estimate_value=sample_sum/N;
-        stratified_y(idx)=abs(estimate_value-true_value);
-    end
+    % for idx=1:a_size
+    %     N=x(idx);
+    %     sample_sum=0;
+    %     for u=1:N
+    %         [ux,uy]=stratified(u,sqrt(N),sqrt(N));
+    %         sample_sum = sample_sum+circle_func(ux,uy);
+    %     end
+    %     estimate_value=sample_sum/N;
+    %     stratified_y(idx)=abs(estimate_value-true_value);
+    % end
 
 
     for idx=1:a_size
@@ -91,25 +94,26 @@ function plot_integrate_circle_func()
     end
 
 
-    #plot(x,halton_y,"-",x,r2_y,"r.",x,stratified_y,"-",x,hammersley_y,"g.");
-    plot(x,halton_y,"b-")
-    hold on
-    plot(x,r2_y,"r-")
+    loglog(x,random_y,"ro",x,halton_y,"--",x,r2_y,":",x,hammersley_y,".",x,hypercube_y,"-");
+    legend('random','halton','r2','hammersley','hypercube')
+    % plot(x,halton_y,"b-")
+    % hold on
+    % plot(x,r2_y,"r-")
 
-    plot(x,stratified_y,"go")
+    % #plot(x,stratified_y,"go")
 
-    plot(x,hammersley_y,"g-")
+    % plot(x,hammersley_y,"g-")
 
-    plot(x,hypercube_y,"-")
+    % plot(x,hypercube_y,"-")
 
-    plot(x,random_y,"ro")
-    hold off
+    % plot(x,random_y,"ro")
+    % hold off
 
     xlabel("sample number");
     ylabel("integration error");
     title(["circle function"]);
-    legend({'halton','r2','stratified','hammersley','hypercube','random'})
+
     xlim([0 x(a_size)])
-    ylim([0.0 0.3])
+    ylim([0.0 0.5])
     print -dpng circle_function.png
 end
