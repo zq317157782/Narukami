a9df97229d86572c2fc5b4ecf7507eb6dd6e0767
zhuqian <zq317157782@163.com>
2019-05-24 21:53:24 +0800

    refactor

--- octave/sampling/circle_func.m
+++ octave/sampling/circle_func.m
@@ -1,9 +1,5 @@
 function z=circle_func(x,y)
-    x = x - 0.5;
-    y = y - 0.5;
-    if (sqrt(x*x+y*y)<=0.5)
-        z=1;
-    else
-        z=0;
-    end
+    x = x .- 0.5;
+    y = y .- 0.5;
+    z=sqrt(x.*x.+y.*y)<=0.5;
 end
--- octave/sampling/plot_integration_error_circle_func.m
+++ octave/sampling/plot_integration_error_circle_func.m
@@ -4,98 +4,36 @@ function plot_integration_error_circle_func(t_size,t_step)
     a_size = t_size/t_step;
 
     x = 1:t_step:t_size;
-    halton_y = zeros(a_size,1);
-    r2_y = zeros(a_size,1);
-    stratified_y = zeros(a_size,1);
-    hammersley_y = zeros(a_size,1);
-    hypercube_y = zeros(a_size,1);
-    random_y = zeros(a_size,1);
+    data = zeros(5,a_size);
+    ux=zeros(5,1);
+    uy=zeros(5,1);
 
     for idx=1:a_size
         N=x(idx);
-        sample_sum=0;
-        for u=1:N
-            [ux,uy]=halton_seq(u);
-            sample_sum = sample_sum+circle_func(ux,uy);
-        end
-        estimate_value=sample_sum/N;
-        halton_y(idx)=abs(estimate_value-true_value);
-    end
-
-
-    for idx=1:a_size
-        N=x(idx);
-        sample_sum=0;
-        for u=1:N
-            [ux,uy]=r2_seq(u);
-            sample_sum = sample_sum+circle_func(ux,uy);
-        end
-        estimate_value=sample_sum/N;
-        r2_y(idx)=abs(estimate_value-true_value);
-    end
-
-    % for idx=1:a_size
-    %     N=x(idx);
-    %     sample_sum=0;
-    %     for u=1:N
-    %         [ux,uy]=stratified(u,sqrt(N),sqrt(N));
-    %         sample_sum = sample_sum+circle_func(ux,uy);
-    %     end
-    %     estimate_value=sample_sum/N;
-    %     stratified_y(idx)=abs(estimate_value-true_value);
-    % end
-
-
-    for idx=1:a_size
-        N=x(idx);
-        sample_sum=0;
-        for u=1:N
-            [ux,uy]=hammersley_set(u,N);
-            sample_sum = sample_sum+circle_func(ux,uy);
-        end
-        estimate_value=sample_sum/N;
-        hammersley_y(idx)=abs(estimate_value-true_value);
-    end
-
-    
-
-    for idx=1:a_size
-        N=x(idx);
-        sample_sum=0;
+        sample_sum=zeros(5,1);
         px=permutate1D(N);
         py=permutate1D(N);
         for u=1:N
-            [ux,uy]=hypercube(u,N,px,py);
-            sample_sum = sample_sum+circle_func(ux,uy);
+            [ux(1),uy(1)]=halton_seq(u);
+            [ux(2),uy(2)]=r2_seq(u);
+            [ux(3),uy(3)]=hammersley_set(u,N);
+            [ux(4),uy(4)]=hypercube(u,N,px,py);
+            ux(5)=rand();
+            uy(5)=rand();
+            sample_sum = sample_sum.+circle_func(ux,uy);
         end
-        estimate_value=sample_sum/N;
-        hypercube_y(idx)=abs(estimate_value-true_value);
+        estimate_value=sample_sum./N;
+        data(:,idx)=abs(estimate_value.-true_value);
     end
 
+    loglog(x,data(1,:),".",x,data(2,:),".",x,data(3,:),".",x,data(4,:),".",x,data(5,:),"ro");
+    legend('halton','r2','hammersley','hypercube','random');
 
-    
+     xlabel("sample number");
+     ylabel("integration error");
+     title(["circle function"]);
 
-    for idx=1:a_size
-        N=x(idx);
-        sample_sum=0;
-        for u=1:N
-            ux=rand();
-            uy=rand();
-            sample_sum = sample_sum+circle_func(ux,uy);
-        end
-        estimate_value=sample_sum/N;
-        random_y(idx)=abs(estimate_value-true_value);
-    end
-
-
-    loglog(x,random_y,"ro",x,halton_y,"--",x,r2_y,":",x,hammersley_y,".",x,hypercube_y,"-");
-    legend('random','halton','r2','hammersley','hypercube')
-
-    xlabel("sample number");
-    ylabel("integration error");
-    title(["circle function"]);
-
-    xlim([0 x(a_size)])
-    ylim([0.0 0.5])
-    print -dpng circle_function.png
+    %  %xlim([0 x(a_size)])
+    %  %ylim([0.0 0.5])
+    %  print -dpng circle_function.png
 end
