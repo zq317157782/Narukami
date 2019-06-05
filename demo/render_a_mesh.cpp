971c37675e0eade2aad8a0b9325730bbcd2a6cf1
zhuqian <zhuqian@fantablade.com>
2019-06-05 15:33:47 +0800

    change to use sampler

--- demo/render_a_mesh.cpp
+++ demo/render_a_mesh.cpp
@@ -3,42 +3,58 @@
 #include "core/transform.h"
 #include "core/mesh.h"
 #include "core/imageio.h"
+#include "core/sampler.h"
 using namespace narukami;
-int main(){
-    
+int main()
+{
 
-   
-     Point3f vertices[4]={Point3f(0,1,1),Point3f(0,0,1),Point3f(1,0,1),Point3f(1,1,1)};
-     Point2f uvs[4] = {Point2f(0,1),Point2f(0,0),Point2f(1,0),Point2f(1,1)};
-     uint32_t indices[6]={0,1,3,1,2,3};
-     auto transform = translate(Vector3f(0,0,0));
-     auto transform2 = translate(Vector3f(0,0,0));
-     auto triangles=create_mesh_triangles(&transform,&transform2,2,indices,4,vertices,nullptr,uvs);
-    
-     auto soa_triangles=cast2SoA(triangles,0,2);
-    std::cout<<soa_triangles[0];
+    Sampler sampler(16);
+
+    Point3f vertices[4] = {Point3f(0, 1, 1), Point3f(0, 0, 1), Point3f(1, 0, 1), Point3f(1, 1, 1)};
+    Point2f uvs[4] = {Point2f(0, 1), Point2f(0, 0), Point2f(1, 0), Point2f(1, 1)};
+    uint32_t indices[6] = {0, 1, 3, 1, 2, 3};
+    auto transform = translate(Vector3f(0, 0, 0));
+    auto transform2 = translate(Vector3f(0, 0, 0));
+    auto triangles = create_mesh_triangles(&transform, &transform2, 2, indices, 4, vertices, nullptr, uvs);
+
+    auto soa_triangles = cast2SoA(triangles, 0, 2);
+    std::cout << soa_triangles[0];
 
     std::vector<uint8_t> image;
-    float data[128*128*3];
-	for (int i = 0; i<128*128; ++i) {
-		narukami::Point2f uv;
-		float t;
-        narukami::SoARay ray(narukami::Point3f((i/128.0f)/128.0f,(i%128)/128.0f,0),narukami::Vector3f(0,0,1));
-        int index;
-        bool b =intersect(ray,soa_triangles[0],&t,&uv,&index);
-       // std::cout<<index;
-        // for(int j=0;j<soa_triangles.size();++j){
-        //     b=(b||intersect(ray,soa_triangles[j],&t,&uv,&index));
-        //     if(b){
-        //         break;
-        //     }
-        // }
-        uv=triangles[index]->sample_uv(uv);
-        data[i*3] = uv[0];
-        data[i*3+1] = uv[1];
-        data[i*3+2] = 0; 
-	}
+    float data[128 * 128 * 3];
+    for (int y = 0; y < 128; ++y)
+    {
+        for (int x = 0; x < 128; ++x)
+        {
+            data[(y*128+x)* 3] = 0;
+            data[(y*128+x)* 3 + 1] = 0;
+            sampler.start_pixel(Point2i(0, 0));
+            do
+            {
+                auto sample = sampler.get_2D();
+                SoARay ray(Point3f((x+sample.x)/128.0f, (y+sample.y)/128.0f, 0), Vector3f(0, 0, 1));
+                Point2f uv;
+                float t;
+                int index;
+                bool b = intersect(ray, soa_triangles[0], &t, &uv, &index);
+                if (b)
+                {
+                    uv = triangles[index]->sample_uv(uv);
+                    data[(y*128+x)* 3] += uv[0];
+                    data[(y*128+x)* 3 + 1] += uv[1];
+                }
+                else
+                {
+                    std::cout << "miss " << ray.o << std::endl;
+                }
 
-    narukami::write_image_to_file("mesh.png",data,128,128);
-	//unsigned error = lodepng::encode(, image, 128, 128);
+            } while (sampler.start_next_sample());
+            data[(y*128+x)* 3] /= 16;
+            data[(y*128+x)* 3 + 1] /= 16;
+            data[(y*128+x)* 3 + 2] = 0;
+        }
+    }
+
+    narukami::write_image_to_file("mesh.png", data, 128, 128);
+    //unsigned error = lodepng::encode(, image, 128, 128);
 }
