78720ecb8c70dfb21cbb2c4e274e8e60fd2ca90e
zhuqian <zhuqian@fantablade.com>
2019-06-13 13:30:52 +0800

    add output function

--- src/core/mesh.h
+++ src/core/mesh.h
@@ -50,7 +50,11 @@ NARUKAMI_BEGIN
         inline const Point3f& operator[](const int i)const { assert(i>=0&&i<2); return _mesh->vertices[_index[i]];}
         inline  Point2f get_vertex_uv(const int i) const{ assert(i>=0&&i<2); if(_mesh->uvs){ return _mesh->uvs[_index[i]]; } return Point2f(0);}
         inline  Point2f sample_uv(const Point2f& u) const{ if(_mesh->uvs){ return _mesh->uvs[_index[0]]*(1.0f-u.x-u.y)+_mesh->uvs[_index[1]]*u.x+_mesh->uvs[_index[2]]*u.y; } return Point2f(0); }
+        friend inline  std::ostream &operator<<(std::ostream &out, const MeshTriangle &v) { out << '(' << v._mesh->vertices[v._index[0]] << ',' << v._mesh->vertices[v._index[1]] << ',' << v._mesh->vertices[v._index[2]] << ')'; return out; }
     };
+
+
+    
 
     inline bool intersect(const Ray& ray,const MeshTriangle& triangle,float* t,Point2f* uv){
         auto v0 = triangle[0];
@@ -62,7 +66,6 @@ NARUKAMI_BEGIN
     std::vector<SoATriangle> cast2SoA(const std::vector<std::shared_ptr<MeshTriangle>>& triangles,uint32_t start,uint32_t count);
 
     std::vector<std::shared_ptr<MeshTriangle>> create_mesh_triangles(const Transform* object2wrold,const Transform* world2object,int triangle_num,const uint32_t *indices,int vertex_num,const Point3f *vertices,const Normal3f *normals=nullptr,const Point2f *uvs=nullptr);
-
 
 
 NARUKAMI_END
