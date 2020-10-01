5be1c8b360e23486772cfcd4a456f1a66a42d8a6
zhuqian <zq317157782@163.com>
2020-10-01 19:00:01 +0800

    [add] 添加file.h

---
+++ src/core/file.h
@@ -0,0 +1,85 @@
+
+/*
+MIT License
+
+Copyright (c) 2019 ZhuQian
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+#include "core/narukami.h"
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <cstring>
+#include <iterator>
+NARUKAMI_BEGIN
+//读取二进制文件
+inline std::vector<uint8_t> read_file_binary(const char* path)
+{
+    std::ifstream file(path, std::ios::binary);
+    std::vector<uint8_t> file_byte_buffer;
+
+    if (file.is_open())
+    {
+        file.seekg(0, std::ios::end);
+        size_t size_bytes = file.tellg();
+        file.seekg(0, std::ios::beg);
+        file_byte_buffer.resize(size_bytes);
+        if (file.read((char*)file_byte_buffer.data(), size_bytes)) return file_byte_buffer;
+    }
+    //else throw std::runtime_error("could not open binary ifstream to path " + path);
+    return file_byte_buffer;
+}
+
+struct memory_buffer : public std::streambuf
+{
+    char * p_start {nullptr};
+    char * p_end {nullptr};
+    size_t size;
+
+    memory_buffer(char const * first_elem, size_t size)
+        : p_start(const_cast<char*>(first_elem)), p_end(p_start + size), size(size)
+    {
+        setg(p_start, p_start, p_end);
+    }
+
+    pos_type seekoff(off_type off, std::ios_base::seekdir dir, std::ios_base::openmode which) override
+    {
+        if (dir == std::ios_base::cur) gbump(static_cast<int>(off));
+        else setg(p_start, (dir == std::ios_base::beg ? p_start : p_end) + off, p_end);
+        return gptr() - p_start;
+    }
+
+    pos_type seekpos(pos_type pos, std::ios_base::openmode which) override
+    {
+        return seekoff(pos, std::ios_base::beg, which);
+    }
+};
+
+struct memory_stream : virtual memory_buffer, public std::istream
+{
+    memory_stream(char const * first_elem, size_t size)
+        : memory_buffer(first_elem, size), std::istream(static_cast<std::streambuf*>(this)) {}
+};
+NARUKAMI_END
